c2556e6952017637205fed015fd8ce270541ceb8
Shantanu <sgkandale@gmail.com>
2024-08-12 15:33:36 +0000

    accept and reject friend request

--- server/errors.go
+++ server/errors.go
@@ -1,14 +1,17 @@
 package server
 
 var (
-	Err_ReadingRequest          = GeneralResponse{Message: "error reading request"}
-	Err_UserAlreadyRegistered   = GeneralResponse{Message: "user already registered"}
-	Err_UserNotFound            = GeneralResponse{Message: "user not found"}
-	Err_UserIdMissing           = GeneralResponse{Message: "user_id is missing"}
-	Err_FriendshipNotFound      = GeneralResponse{Message: "friendship not found"}
-	Err_FriendshipAlreadyExists = GeneralResponse{Message: "friendship already exists"}
-	Err_SomethingWrong          = GeneralResponse{Message: "something went wrong"}
-	Err_AuthHeaderMissing       = GeneralResponse{Message: "'Authorization' header is missing"}
+	Err_ReadingRequest            = GeneralResponse{Message: "error reading request"}
+	Err_UserAlreadyRegistered     = GeneralResponse{Message: "user already registered"}
+	Err_UserNotFound              = GeneralResponse{Message: "user not found"}
+	Err_UserIdMissing             = GeneralResponse{Message: "user_id is missing"}
+	Err_FriendshipNotFound        = GeneralResponse{Message: "friendship not found"}
+	Err_FriendshipAlreadyExists   = GeneralResponse{Message: "friendship already exists"}
+	Err_RequestIdMissing          = GeneralResponse{Message: "request_id is missing"}
+	Err_RequestIdInvalid          = GeneralResponse{Message: "request_id is invalid"}
+	Err_FriendshipRequestNotFound = GeneralResponse{Message: "friendship request not found"}
+	Err_SomethingWrong            = GeneralResponse{Message: "something went wrong"}
+	Err_AuthHeaderMissing         = GeneralResponse{Message: "'Authorization' header is missing"}
 )
 
 var (
--- server/handlersFriends.go
+++ server/handlersFriends.go
@@ -3,6 +3,8 @@ package server
 import (
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 
 	"socialite/database"
 
@@ -110,6 +112,114 @@ func (s *Server) SendFriendRequest(ginCtx *gin.Context) {
 	err = s.db.PutFriendship(ginCtx, friendshipInstance)
 	if err != nil {
 		log.Printf("[ERROR] server.SendFriendRequest: creating friendship: %s", err.Error())
+		ginCtx.JSON(http.StatusInternalServerError, Err_SomethingWrong)
+		return
+	}
+
+	ginCtx.JSON(http.StatusOK, Resp_Success)
+}
+
+func (s *Server) AcceptFriendRequest(ginCtx *gin.Context) {
+	// get user from context
+	user, exists := ginCtx.Get(Header_AuthUserKey)
+	if !exists || user == nil {
+		ginCtx.JSON(http.StatusUnauthorized, Err_AuthHeaderMissing)
+		return
+	}
+	userInstance := user.(*database.User)
+
+	// get request id from path
+	requestId := ginCtx.Param("request_id")
+	if requestId == "" {
+		ginCtx.JSON(http.StatusBadRequest, Err_RequestIdMissing)
+		return
+	}
+
+	requestIdInt, err := strconv.Atoi(requestId)
+	if err != nil {
+		ginCtx.JSON(http.StatusBadRequest, Err_RequestIdInvalid)
+		return
+	}
+
+	friendshipInstance, err := s.db.GetFriendshipById(ginCtx, int32(requestIdInt))
+	if err != nil {
+		if err == database.Err_NotFound {
+			ginCtx.JSON(http.StatusNotFound, Err_FriendshipRequestNotFound)
+			return
+		}
+		log.Printf("[ERROR] server.AcceptFriendRequest: getting friendship request: %s", err.Error())
+		ginCtx.JSON(http.StatusInternalServerError, Err_SomethingWrong)
+		return
+	}
+
+	if friendshipInstance.User1 != userInstance.Name && friendshipInstance.User2 != userInstance.Name {
+		ginCtx.JSON(http.StatusNotFound, Err_FriendshipRequestNotFound)
+		return
+	}
+
+	friendshipInstance.Status = database.Friendship_Status_Confirmed
+	friendshipInstance.UpdatedAt = time.Now()
+
+	// get friendship details from db
+	err = s.db.UpdateFriendship(ginCtx, friendshipInstance)
+	if err != nil {
+		if err == database.Err_NotFound {
+			ginCtx.JSON(http.StatusNotFound, Err_FriendshipRequestNotFound)
+			return
+		}
+		log.Printf("[ERROR] server.AcceptFriendRequest: getting friendship request: %s", err.Error())
+		ginCtx.JSON(http.StatusInternalServerError, Err_SomethingWrong)
+		return
+	}
+
+	ginCtx.JSON(http.StatusOK, Resp_Success)
+}
+
+func (s *Server) RejectFriendRequest(ginCtx *gin.Context) {
+	// get user from context
+	user, exists := ginCtx.Get(Header_AuthUserKey)
+	if !exists || user == nil {
+		ginCtx.JSON(http.StatusUnauthorized, Err_AuthHeaderMissing)
+		return
+	}
+	userInstance := user.(*database.User)
+
+	// get request id from path
+	requestId := ginCtx.Param("request_id")
+	if requestId == "" {
+		ginCtx.JSON(http.StatusBadRequest, Err_RequestIdMissing)
+		return
+	}
+
+	requestIdInt, err := strconv.Atoi(requestId)
+	if err != nil {
+		ginCtx.JSON(http.StatusBadRequest, Err_RequestIdInvalid)
+		return
+	}
+
+	friendshipInstance, err := s.db.GetFriendshipById(ginCtx, int32(requestIdInt))
+	if err != nil {
+		if err == database.Err_NotFound {
+			ginCtx.JSON(http.StatusNotFound, Err_FriendshipRequestNotFound)
+			return
+		}
+		log.Printf("[ERROR] server.RejectFriendRequest: getting friendship request: %s", err.Error())
+		ginCtx.JSON(http.StatusInternalServerError, Err_SomethingWrong)
+		return
+	}
+
+	if friendshipInstance.User1 != userInstance.Name && friendshipInstance.User2 != userInstance.Name {
+		ginCtx.JSON(http.StatusNotFound, Err_FriendshipRequestNotFound)
+		return
+	}
+
+	err = s.db.DeleteFriendship(ginCtx, friendshipInstance.Id)
+	if err != nil {
+		if err == database.Err_NotFound {
+			ginCtx.JSON(http.StatusNotFound, Err_FriendshipRequestNotFound)
+			return
+		}
+		log.Printf("[ERROR] server.RejectFriendRequest: deleting friendship request: %s", err.Error())
 		ginCtx.JSON(http.StatusInternalServerError, Err_SomethingWrong)
 		return
 	}
