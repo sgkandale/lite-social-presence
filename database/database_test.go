44c941eb2f912d4ab2bc8ff15d77039ee6e4f491
Shantanu <sgkandale@gmail.com>
2024-08-12 13:45:41 +0000

    postgres users method implemented

--- database/database_test.go
+++ database/database_test.go
@@ -2,7 +2,9 @@ package database_test
 
 import (
 	"context"
+	"log"
 	"testing"
+	"time"
 
 	"socialite/config"
 	"socialite/database"
@@ -27,4 +29,39 @@ func TestNew(t *testing.T) {
 	if dbConn == nil {
 		t.Error("dbConn is nil")
 	}
+}
+
+func TestPutUser(t *testing.T) {
+	err := dbConn.PutUser(
+		context.Background(),
+		&database.User{
+			Name:      "user1",
+			CreatedAt: time.Now(),
+		},
+	)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestGetUser(t *testing.T) {
+	user, err := dbConn.GetUser(
+		context.Background(),
+		"user1",
+	)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if user == nil {
+		t.Error("user is nil")
+		return
+	}
+	if user.Name != "user1" {
+		t.Error("user name is incorrect")
+	}
+	if user.CreatedAt.IsZero() {
+		t.Error("user created at is zero")
+	}
+	log.Printf("user : %+v", user)
 }
--- database/postgres/user.go
+++ database/postgres/user.go
@@ -2,13 +2,64 @@ package postgres
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	"socialite/database"
+
+	"github.com/jackc/pgx/v5/pgconn"
 )
 
 func (c *Client) PutUser(ctx context.Context, user *database.User) error {
+	if user == nil {
+		return errors.New("user input is nil")
+	}
+
+	queryCtx, cancelQueryCtx := context.WithTimeout(ctx, c.timeout)
+	defer cancelQueryCtx()
+
+	_, err := c.Pool.Exec(
+		queryCtx,
+		`INSERT INTO users 
+			(name, created_at) 
+		VALUES 
+			($1, $2)`,
+		user.Name, user.CreatedAt,
+	)
+	if err != nil {
+		// duplicate user name check
+		if pgErr, ok := err.(*pgconn.PgError); ok && pgErr.Code == "23505" {
+			return database.Err_DuplicatePrimaryKey
+		}
+		return fmt.Errorf("executing postgres insertion: %s", err.Error())
+	}
 	return nil
 }
+
 func (c *Client) GetUser(ctx context.Context, name string) (*database.User, error) {
-	return nil, nil
+	if name == "" {
+		return nil, errors.New("name input is empty")
+	}
+
+	queryCtx, cancelQueryCtx := context.WithTimeout(ctx, c.timeout)
+	defer cancelQueryCtx()
+
+	row := c.Pool.QueryRow(
+		queryCtx,
+		`SELECT 
+			created_at
+		FROM users
+		WHERE 
+			name = $1`,
+		name,
+	)
+
+	user := &database.User{
+		Name: name,
+	}
+	err := row.Scan(&user.CreatedAt)
+	if err != nil {
+		return nil, fmt.Errorf("scanning postgres row: %s", err.Error())
+	}
+	return user, nil
 }
