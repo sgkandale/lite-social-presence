d3398eceb0ca28b3090a36e62885cbd7bc7ecd9e
Shantanu <sgkandale@gmail.com>
2024-08-14 17:45:49 +0000

    additional methods in cache interface

--- cache/cache.go
+++ cache/cache.go
@@ -1,3 +1,21 @@
 package cache
 
-type Cache interface{}
+import (
+	"context"
+	"time"
+)
+
+type Cache interface {
+	PutUserOnline(ctx context.Context, userName string) error
+	IsUserOnline(ctx context.Context, userName string) (bool, error)
+
+	PutUserFriendsList(ctx context.Context, userName string, friends []string) error
+	GetUserFriendsList(ctx context.Context, userName string) ([]string, error)
+
+	PutPartyMembersList(ctx context.Context, partyName string, members []string) error
+	GetPartyMembersList(ctx context.Context, partyName string) ([]string, error)
+}
+
+const (
+	UserOnlineExpiry = time.Second * 10
+)
